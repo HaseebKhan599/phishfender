37a12f114e334c35015f920fcccd115f66e0ac43
Abdul Haseeb Khan <abdulhaseebkhan499@gmail.com>
2024-04-27 15:01:21 +0500

    heading adding to the header component and bootstrap grid

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,20 @@ import Styles from "./header.module.css";
 const header = ()=>{
     return (
         <div id={Styles.headerDiv}>
-            <h2 id={Styles.title}>PhishFender</h2>
+            <div id={Styles.gridContainer} class="container">
+                <div id={Styles.gridRow1} class="row">
+                    <div id={Styles.gridCol1} class="col-sm-4">
+                        <h2 id={Styles.title}>PhishFender</h2>
+                    </div>
+                    <div class="col-sm-3">
+
+                    </div>
+                    <div class="col-sm-5">
+                    
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 }
